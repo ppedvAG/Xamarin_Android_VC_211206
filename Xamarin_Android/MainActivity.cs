a640eeee9e37a05876bb95c6a3b323411cd41ab5
KlaasHagge <klaash@ppedv.de>
2021-12-06 15:32:57 +0100

    Projektdateien hinzufügen.

---
+++ Xamarin_Android/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace Xamarin_Android
+{
+    //Jede Android-Activity steht für eine Aktion oder ein Layout, welche durch die App durchgeführt
+    //oder angezeigt wird. Der 'Code Behind' einer Activity ist eine C#-Klasse, welche mit dem 'Activity'
+    //-Attribut gekennzeichnet ist. Hier kann auch der evtl. angezeigte Titel und der verwendete Style
+    //definiert werden.
+    //Soll die Activity die zuerst angezeigte Activity (=Startseite) der App sein, muss hier die Property
+    //'MainLauncher' auf true stehen.
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        //Properties, in welchen die Steuerelemente des Layouts für den Zugriff durch C# abgelegt werden
+        public Button Btn_Ok { get; set; }
+        public Button Btn_Google { get; set; }
+        public Button Btn_Picture { get; set; }
+        public EditText Edt_Input { get; set; }
+
+        //Methode, welche beim Starten (Initialisieren) der Activity ausgeführt wird
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //Aufruf der Base-OnCreate()-Methode (Grundlegende Activity-Initialisierung)
+            base.OnCreate(savedInstanceState);
+            //Initialisierung der Xamarin-Essentials
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            //Zuweisung und Aktivierung eines Layouts (aus dem layout-Ordner) zu dieser Activity. Dies
+            //erfolgt mittels der Ressourcen-Klassen).
+            SetContentView(Resource.Layout.activity_main);
+
+            //Zuweisung der UI-Elemente zu den Properties mittels der FindViewById<>()-Methode,
+            //welche die Resource-Klassen nach der angegebenen Id durchsucht.
+            Btn_Ok = FindViewById<Button>(Resource.Id.activity_main_Btn_Ok);
+            Edt_Input = FindViewById<EditText>(Resource.Id.activity_main_Edt_Input);
+            Btn_Google = FindViewById<Button>(Resource.Id.activity_main_Btn_Google);
+            Btn_Picture = FindViewById<Button>(Resource.Id.activity_main_Btn_ShowPicture);
+
+            //Zuweisung einer Methode zu einem Click-Event eines Buttons.
+            //Diese Methode kreiert einen Toast (kl. Anzeige am unteren Bildschirmrand) und zeigt ihn an.
+            Btn_Ok.Click += (s, e) => Toast.MakeText(this, $"Ihre gewählte Zahl ist {Edt_Input.Text}.", ToastLength.Long).Show();
+            //EditText.Click wird mit Klick auf den Haken in der Tastatur ausgeführt
+            Edt_Input.Click += (s, e) => Toast.MakeText(this, $"Ihre gewählte Zahl ist {Edt_Input.Text}.", ToastLength.Long).Show();
+
+            //Impliziter Intent (Verweis auf eine Activity, welche mit dem ihrem Typen zugeordneten
+            //Standartprogramm geöffnet wird) am Beispiel eines Webpage-Aufrufs im Standartbrowser.
+
+            //Erstellung des Intents
+            Intent impliziterIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("https://placeimg.com/640/800/any"));
+            //Zuweisung des Click-Events mit der StartActivity()-Methode, welcher der Intent übergeben wird
+            Btn_Google.Click += (s, e) => StartActivity(impliziterIntent);
+
+            //Expliziter Intent (Verweis auf eine Activity, welche in einer genau definierten App ausgeführt wird)
+            //Am Beispiel des Öffnens eines neuen Layouts
+
+            //Erstellung des Intents
+            Intent expliziterIntent = new Intent(this, typeof(ShowPictureActivity));
+            //Zuweisung des Click-Events
+            Btn_Picture.Click += (s, e) => StartActivity(expliziterIntent);
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Xamarin_Android/ShowPictureActivity.cs
@@ -0,0 +1,94 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Xamarin_Android
+{
+    [Activity(Label = "ShowPictureActivity")]
+    public class ShowPictureActivity : Activity
+    {
+        //Control-Properties
+        public EditText EdT_Height { get; set; }
+        public EditText EdT_Width { get; set; }
+        public Button Btn_Go { get; set; }
+        public ImageView Img_Content { get; set; }
+        public ProgressBar PrB_LoadingPicture { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //Aufruf der Base-Methode (Grundlegende Activity-Initialisierung)
+            base.OnCreate(savedInstanceState);
+
+            //Verknüpfen und Öffenen der Layout-Datei (im layout-Ordner)
+            SetContentView(Resource.Layout.activity_showpicture);
+
+            //Zuweisung und Initialisierung der Controls
+            EdT_Height = FindViewById<EditText>(Resource.Id.EdT_Height);
+            EdT_Width = FindViewById<EditText>(Resource.Id.EdT_Width);
+            Btn_Go = FindViewById<Button>(Resource.Id.Btn_Go);
+            Img_Content = FindViewById<ImageView>(Resource.Id.Img_Content);
+            PrB_LoadingPicture = FindViewById<ProgressBar>(Resource.Id.PrB_LoadingPicture);
+
+            //Zuweisung einer "Weiterleitung" zum jeweils nächsten Control durch den Bestätigen-Button im Eingabefenster
+            //Focus auf nächsten EditText (EdT_Height)
+            EdT_Height.Click += (s, e) => EdT_Width.RequestFocus();
+            //Markieren des Inhalts von EdT_Height
+            EdT_Height.Click += (s, e) => EdT_Width.SelectAll();
+            //Ausführen der Btn_Go.Click-Methode
+            EdT_Width.Click += GetRandomPic;
+            //Verbergen der Tastatur
+            EdT_Width.Click += (s, e) =>
+            {
+                InputMethodManager imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+                imm.HideSoftInputFromWindow(EdT_Width.WindowToken, 0);
+            };
+
+            //Zuweisung einer Methode zum Click-Event des Buttons
+            Btn_Go.Click += GetRandomPic;
+        }
+
+        private async void GetRandomPic(object sender, EventArgs e)
+        {
+            //Parsen der durch den Benutzer eingegebenen Höhe und Breite
+            int breite = int.Parse(EdT_Width.Text);
+            int hoehe = int.Parse(EdT_Height.Text);
+
+            //Initialisieren des ProgressDialogs
+            //ProgressDialog progressDialog = new ProgressDialog(this);
+            //progressDialog.SetMessage("Downloading Picture...");
+
+            Img_Content.Visibility = ViewStates.Gone;
+            PrB_LoadingPicture.Visibility = ViewStates.Visible;
+
+            //Öffnen des WebClients
+            using (WebClient client = new WebClient())
+            {
+                //Öffnen des ProgressDialogs
+                //progressDialog.Show();
+
+                //Herunterladen des Bilds als Byte-Array
+                byte[] bild = await client.DownloadDataTaskAsync($"http://placeimg.com/{breite}/{hoehe}/any");
+                //Umwandlung des Arrays in ein Bitmap
+                Android.Graphics.Bitmap bitmap = Android.Graphics.BitmapFactory.DecodeByteArray(bild, 0, bild.Length);
+
+                //Setzen des Bildes in den ImageView
+                Img_Content.SetImageBitmap(bitmap);
+
+                //Schließen des ProgressDialogs
+                //progressDialog.Dismiss();
+            };
+
+            PrB_LoadingPicture.Visibility = ViewStates.Gone;
+            Img_Content.Visibility = ViewStates.Visible;
+        }
+    }
+}
